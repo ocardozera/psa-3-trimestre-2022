d1bee3ea738c5d9fb0883c28555d08cb230a3ff8
Matheus Cardoso <matheusjosecardoso@outlook.com>
2022-09-23 20:35:39 -0300

    commit inicial

---
+++ lib/domain/entidade/cliente.dart
@@ -0,0 +1,19 @@
+class Cliente {
+  late String nome;
+  late String email;
+  late String cpf;
+  late Cnh cnh;
+  late DateTime dataNascimento;
+
+  Cliente(
+      {required this.nome,
+      required this.cpf,
+      required this.cnh,
+      required this.dataNascimento});
+
+
+  Cliente.cadastrar()
+
+
+}
+
